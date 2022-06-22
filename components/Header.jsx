a3db943d3df167e0d5de8098180faa966d388213
abhiram11 <abhiramsatpute@gmail.com>
2022-06-22 11:07:17 +0530

    added header with toggle for hamburger

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
 
 const Header = () => {
+  const [header, setHeader] = useState(false);
+  const handleClick = () => setHeader(!header);
+
   return (
     <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
       Header
@@ -14,11 +17,17 @@ const Header = () => {
         <li>Contact</li>
       </ul>
       {/* Hamburger Menu */}
-      <div className="md:hidden z-10">
-        <FaBars />
+      <div onClick={handleClick} className="md:hidden z-10">
+        {!header ? <FaBars /> : <FaTimes />}
       </div>
       {/* Mobile Menu */}
-      <ul className="hidden absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center">
+      <ul
+        className={
+          !header
+            ? "hidden"
+            : "absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center"
+        }
+      >
         <li className="py-6 text-4xl ">Home</li>
         <li className="py-6 text-4xl ">About</li>
         <li className="py-6 text-4xl ">Skills</li>
